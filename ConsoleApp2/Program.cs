5625fe054eecbd1bc3a577dd62114e165f0e1778
Zbiku <Zbiku@MSI>
2023-10-19 22:53:45 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        //a
+        /*int x = 1, y = 4, z = 2;
+        bool wynik = (x++ > 1 && y++ == 4 && z-- > 0);
+        Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+        Console.ReadKey();*/
+        //b
+        /*int x = 1, y = 4, z = 2;
+        bool wynik = (x++ > 1 & y++ == 4 && z-- > 0);
+        Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+        Console.ReadKey();*/
+        //c
+        /*int x = 1, y = 4, z = 2;
+        bool wynik = (x++ > 1 & y++ == 4 & z-- > 0);
+        Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+        Console.ReadKey();*/
+        //d
+        /*int x = 1, y = 4, z = 2;
+        bool wynik = (x++ == 1 || y++ > 2 || ++z > 0);
+        Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+        Console.ReadKey();*/
+        //e
+        /*int x = 1, y = 4, z = 2;
+        bool wynik = (x++ == 1 | y++ > 2 || ++z > 0);
+        Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+        Console.ReadKey();*/
+        //f
+        /*int x = 1, y = 4, z = 2;
+        bool wynik = (x++ == 1 | y++ > 2 | ++z > 0);
+        Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+        Console.ReadKey();*/
+    }
+}
